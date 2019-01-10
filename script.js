013e71dad4435e442dc5cbd83a5b67736ce19583
David Gonzalez <davidg1739@hstat.org>
2019-01-10 15:26:29 +0000

    Add bootstrap and jquery link and script tags to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function () {
+  
+    $.get("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY", {
+      key: AIzaSyDuxPyuZ_uvAL3F9bCDGqBEY1I-COIYGeQ
+
+    }).then(result => {
+      tokenOfPage = info.nextPageToken;
+    })
+})
