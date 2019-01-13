83b1699607409e0cb05fad12765e3e22f4bce74a
David Gonzalez <davidg1739@hstat.org>
2019-01-13 03:09:50 +0000

    Add user location function

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-$(function () {
-  
+$(function() {
+
     // $.get("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY", {
     //   key: AIzaSyDuxPyuZ_uvAL3F9bCDGqBEY1I-COIYGeQ
 
     // }).then(result => {
     //   tokenOfPage = info.nextPageToken;
     // })
+    
+    function showPosition (position) {
+        console.log(position)
+    }
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                showPosition,
+                function () {
+                    console.log('Error')
+                },
+                {enableHighAccuracy: true}
+                )
+        }
+        else {
+            console.log('Error');
+        }
+    }
+    
+    getLocation();
 })
