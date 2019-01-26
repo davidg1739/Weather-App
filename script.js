dc0541e6cc7f9ade68db605324febff6f0c520ba
David Gonzalez <davidg1739@hstat.org>
2019-01-26 03:57:38 +0000

    Add wind speed and sunrise and sunset info

--- script.js
+++ script.js
@@ -197,14 +197,14 @@ $(function() {
         }
 
         if (inputtedTime[0] < 12) {
-            t = `${inputtedTime[0]}` + ':' + `${mins}` + 'am';
+            t = `${inputtedTime[0]}` + ':' + `${mins}` + ' AM';
         }
         else if (inputtedTime[0] == 12) {
-            t = `${inputtedTime[0]}` + ':' + `${mins}` + 'pm';
+            t = `${inputtedTime[0]}` + ':' + `${mins}` + ' PM';
         }
         else if (inputtedTime[0] > 12) {
             let tempOperation = inputtedTime[0] % 12;
-            t = `${tempOperation}` + ':' + `${mins}` + 'pm';
+            t = `${tempOperation}` + ':' + `${mins}` + ' PM';
         }
         else {
             let tempOperation = inputtedTime[0] / 2;
@@ -243,19 +243,101 @@ $(function() {
         $('.resultsList').css('display', 'none');
         $('.locationInfo').html(`Lattitude: ${coords.lat} Longitude: ${coords.long}`)
 
-        $('.wordTemperature').css('display', 'block');
 
         $('.weather-temperature').openWeather({
             key: "75735e198dd34b697621802ee7001b9e",
             lat: coords.lat,
             lng: coords.long,
             descriptionTarget: ".desc",
+            windSpeedTarget: ".windSpeed",
+            sunriseTarget: ".sunriseTime",
+            sunsetTarget: ".sunsetTime",
             success: weatherOutput
         });
 
         function weatherOutput(result) {
             console.log(result);
 
+            var start = new Date();
+            
+            let currentTime = getDateAndTime();
+            currentTime = currentTime[4];
+            currentTime = currentTime.split(" ");
+            currentTime[0].split(":")
+            
+            
+            
+            
+            var sunrise = new Date();
+            let sunriseTimeForResult = result.sunrise;
+            sunriseTimeForResult = sunriseTimeForResult.split(":");
+            if (currentTime[1] == "PM" || currentTime[0][0] > sunriseTimeForResult[0]) {
+                sunrise.setDate(start.getDate() + 1);
+            }
+            sunriseTimeForResult[1] = sunriseTimeForResult[1].split(" ");
+            if (sunriseTimeForResult[1][1] == "AM") {
+                sunriseTimeForResult[0] = sunriseTimeForResult[0];
+            }
+            else if (sunriseTimeForResult[1][1] == "PM") {
+                sunriseTimeForResult[0] = new String ((Number(sunriseTimeForResult[0]) + 12));
+            }
+            sunrise.setHours(sunriseTimeForResult[0], sunriseTimeForResult[1][0]);
+            
+            let timeDiffSr = 0;
+            let hours = 1000 * 60 * 60;
+            timeDiffSr = ((sunrise - start) / hours);
+            timeDiffSr = new String (timeDiffSr).split(".");
+            timeDiffSr[0] = timeDiffSr[0] + " hours ";
+            timeDiffSr[1] = "." + timeDiffSr[1];
+            timeDiffSr[1] = new String ((Math.floor(Number(timeDiffSr[1]) * 60))) + " minutes";
+            
+            var sunset = new Date();
+            sunset.setDate(start.getDate() + 1);
+            let sunsetTimeForResult = result.sunset;
+            sunsetTimeForResult = sunsetTimeForResult.split(":");
+            if ((currentTime[1] == "PM" && currentTime[0][0] > sunsetTimeForResult[0])) {
+                sunset.setDate(start.getDate() + 1);
+            }
+            sunsetTimeForResult[1] = sunsetTimeForResult[1].split(" ");
+            if (sunsetTimeForResult[1][1] == "AM") {
+                sunsetTimeForResult[0] = sunsetTimeForResult[0];
+            }
+            else if (sunsetTimeForResult[1][1] == "PM") {
+                sunsetTimeForResult[0] = new String ((Number(sunsetTimeForResult[0]) + 12));
+            }
+            sunset.setHours(sunsetTimeForResult[0],sunsetTimeForResult[1][0]);
+            
+            let timeDiffSs = 0;
+            timeDiffSs = ((sunset - start) / hours);
+            timeDiffSs = new String (timeDiffSs).split(".");
+            timeDiffSs[0] = timeDiffSs[0] + " hours ";
+            timeDiffSs[1] = "." + timeDiffSs[1];
+            timeDiffSs[1] = new String ((Math.floor(Number(timeDiffSs[1]) * 60))) + " minutes";
+            
+            let timeDiffs = {
+                sunrise: timeDiffSr,
+                sunset: timeDiffSs
+            };
+            
+            $('.srTime').html(timeDiffs.sunrise);
+            $('.ssTime').html(timeDiffs.sunset);
+            
+
+            let speed = $('.windSpeed').html();
+            speed = speed.split(" ");
+            speed = $(`<span class='speedNumber'>${speed[0]}</span> <span data-toggle="tooltip" title="Meters Per Second">${speed[1]}</span>`);
+            $('.windSpeed').html(speed);
+            
+            let windSpeedMilesPerHour = $('.speedNumber').html();
+            console.log(windSpeedMilesPerHour);
+            windSpeedMilesPerHour = windSpeedMilesPerHour.split(" ");
+            windSpeedMilesPerHour = new String ((Number (windSpeedMilesPerHour[0]) * 2.236936)) + " mph";
+            
+            $('.otherSpeeds').html(windSpeedMilesPerHour)
+            
+            let description = $('.desc').html();
+            description = description.charAt(0).toUpperCase() + description.slice(1);
+            $('.desc').html(description);
             let currentTemperatureCelsius = result.temperature.current;
             let currentTemperatureFarenheight = currentTemperatureCelsius.replace(/\D/g, "");
             currentTemperatureFarenheight = (((9 / 5) * currentTemperatureFarenheight) + 32);
